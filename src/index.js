287724ad04d98d99153eccf605dabfb42b4280df
Ankur Maity <anma20cs@cmrit.ac.in>
2023-06-24 12:34:43 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Grocery from './pages/grocery';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/grocery",
+    element: <Grocery/>,
+  },
+]);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
---
+++ src/pages/grocery.js
@@ -0,0 +1,211 @@
+import React from 'react';
+import './style.css'
+
+function Grocery() {
+  return (
+    <div className="grid-container">
+        
+        <header className="row">
+        <div><a href="index.html" className="brand">Agrawal Store</a></div>
+        <div>
+            <a href="grocery.html">Grocery</a>
+            <a href="snacks.html">Snack</a>
+            <a href="clenHome.html">Clean & Home Essentials</a>
+            <a href="cart.html">Cart</a>
+            <a href="signin.html">Sign In</a>
+        </div>
+    </header>
+    <div className="row center">
+        
+        <div className="card">
+           
+            <a href="product.html">
+                
+                <img className="medium" src="/Grocery Fotos/atta and flours/fortune-atta.jpg" height="200" width="200" alt="Fortune-Atta"/>
+            </a>
+        <div className="card-body">
+            <a href="product.html">
+                <h2>Fortune Atta</h2>
+            </a>
+            <div className="rating">
+                <span> <i className="fa fa-star"></i> </span>
+                <span> <i className="fa fa-star"></i> </span>
+                <span> <i className="fa fa-star"></i> </span>
+                <span> <i className="fa fa-star"></i> </span>
+                <span> <i className="fa fa-star-half"></i> </span>
+            </div>
+            <div className="price">
+                Rs. 300
+            </div>
+       </div>
+    </div>
+    <div className="card">
+        <a href="product.html">
+            <img className="medium" src="/Grocery Fotos/atta and flours/aashirvaad-atta.jpg" height="200" width="200" alt="Aashirvaad-atta"/>
+        </a>
+    <div className="card-body">
+        <a href="product.html">
+            <h2>Aashirvaad Atta</h2>
+        </a>
+        <div className="rating">
+            <span> <i className="fa fa-star"></i> </span>
+            <span> <i className="fa fa-star"></i> </span>
+            <span> <i className="fa fa-star"></i> </span>
+            <span> <i className="fa fa-star"></i> </span>
+            <span> <i className="fa fa-star"></i> </span>
+        </div>
+        <div className="price">
+            Rs. 250
+        </div>
+   </div>
+</div>
+<div className="card">
+    <a href="product.html">
+        <img className="medium" src="/Grocery Fotos/atta and flours/fortune-maida.jpg" height="200" width="200" alt="Fortune-maida"/>
+    </a>
+<div className="card-body">
+    <a href="product.html">
+        <h2>Fortune Maida</h2>
+    </a>
+    <div className="rating">
+        <span> <i className="fa fa-star"></i> </span>
+        <span> <i className="fa fa-star"></i> </span>
+        <span> <i className="fa fa-star"></i> </span>
+        <span> <i className="fa fa-star"></i> </span>
+        <span> <i className="fa fa-star"></i> </span>
+    </div>
+    <div className="price">
+        Rs. 250
+    </div>
+</div>
+</div>
+<div className="card">
+<a href="product.html">
+    <img className="medium" src="/Grocery Fotos/atta and flours/fortune-suji.jpg" height="200" width="200" alt="Fortune-Suji"/>
+</a>
+<div className="card-body">
+<a href="product.html">
+    <h2>Fortune Suji</h2>
+</a>
+<div className="rating">
+    <span> <i className="fa fa-star"></i> </span>
+    <span> <i className="fa fa-star"></i> </span>
+    <span> <i className="fa fa-star"></i> </span>
+    <span> <i className="fa fa-star"></i> </span>
+    <span> <i className="fa fa-star"></i> </span>
+</div>
+<div className="price">
+    Rs. 250
+</div>
+</div>
+</div>
+<div className="card">
+    <a href="product.html">
+        <img className="medium" src="/Grocery Fotos/rice/basmati.jpg" height="200" width="200" alt="Basmati Rice"/>
+    </a>
+    <div className="card-body">
+    <a href="product.html">
+        <h2>Daawat Basmati Rice</h2>
+    </a>
+    <div className="rating">
+        <span> <i className="fa fa-star"></i> </span>
+        <span> <i className="fa fa-star"></i> </span>
+        <span> <i className="fa fa-star"></i> </span>
+        <span> <i className="fa fa-star"></i> </span>
+        <span> <i className="fa fa-star"></i> </span>
+    </div>
+    <div className="price">
+        Rs. 250
+    </div>
+    </div>
+    </div>
+    <div className="card">
+        <a href="product.html">
+
+            <img className="medium" src="/Grocery Fotos/rice/brown-rice.jpg" height="200" width="200" alt="Fortune-Suji"/>
+        </a>
+        <div className="card-body">
+        <a href="product.html">
+            <h2>Fortune Suji</h2>
+        </a>
+        <div className="rating">
+            <span> <i className="fa fa-star"></i> </span>
+            <span> <i className="fa fa-star"></i> </span>
+            <span> <i className="fa fa-star"></i> </span>
+            <span> <i className="fa fa-star"></i> </span>
+            <span> <i className="fa fa-star"></i> </span>
+        </div>
+        <div className="price">
+            Rs. 250
+        </div>
+        </div>
+        </div>
+        <div className="card">
+            <a href="product.html">
+    
+                <img className="medium" src="/Grocery Fotos/rice/white-rice.jpg" height="200" width="200" alt="Fortune-Suji"/>
+            </a>
+            <div className="card-body">
+            <a href="product.html">
+                <h2>Fortune Suji</h2>
+            </a>
+            <div className="rating">
+                <span> <i className="fa fa-star"></i> </span>
+                <span> <i className="fa fa-star"></i> </span>
+                <span> <i className="fa fa-star"></i> </span>
+                <span> <i className="fa fa-star"></i> </span>
+                <span> <i className="fa fa-star"></i> </span>
+            </div>
+            <div className="price">
+                Rs. 250
+            </div>
+            </div>
+            </div>
+            <div className="card">
+                <a href="product.html">
+        
+                    <img className="medium" src="/Grocery Fotos/noodle and pasta/maggi.jpg" height="200" width="250" alt="Fortune-Suji"/>
+                </a>
+                <div className="card-body">
+                <a href="product.html">
+                    <h2>Fortune Suji</h2>
+                </a>
+                <div className="rating">
+                    <span> <i className="fa fa-star"></i> </span>
+                    <span> <i className="fa fa-star"></i> </span>
+                    <span> <i className="fa fa-star"></i> </span>
+                    <span> <i className="fa fa-star"></i> </span>
+                    <span> <i className="fa fa-star"></i> </span>
+                </div>
+                <div className="price">
+                    Rs. 250
+                </div>
+                </div>
+                </div>
+                <div className="card">
+                    <a href="product.html">
+            
+                        <img className="medium" src="/Grocery Fotos/noodle and pasta/macroni.jpg" height="200" width="200" alt="Fortune-Suji"/>
+                    </a>
+                    <div className="card-body">
+                    <a href="product.html">
+                        <h2>Fortune Suji</h2>
+                    </a>
+                    <div className="rating">
+                        <span> <i className="fa fa-star"></i> </span>
+                        <span> <i className="fa fa-star"></i> </span>
+                        <span> <i className="fa fa-star"></i> </span>
+                        <span> <i className="fa fa-star"></i> </span>
+                        <span> <i className="fa fa-star"></i> </span>
+                    </div>
+                    <div className="price">
+                        Rs. 250
+                    </div>
+                    </div>
+                    </div>
+    </div>
+ </div>
+  )
+}
+
+export default Grocery
